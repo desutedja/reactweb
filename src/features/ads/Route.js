e26f9c92f3b5b38131ecd37905b49930369ecdc1
Avid Santiko <avidkucing@gmail.com>
2020-04-21 16:01:04 +0700

    get ads

--- src/features/ads/Route.js
+++ src/features/ads/Route.js
@@ -6,7 +6,19 @@ import Table from '../../components/Table';
 import { getAds } from './slice';
 
 const columns = [
-    { Header: 'Name', accessor: 'name' },
+    { Header: "Title", accessor: "content_name" },
+    {
+        Header: "Target", accessor: row =>
+            row.gender + ", " + row.age_from + "-" + row.age_to + ", " + row.os
+    },
+    { Header: "Priority", accessor: "total_priority_score" },
+    { Header: "Appear As", accessor: "appear_as" },
+    { Header: "Image", accessor: "" },
+    { Header: "Media Type", accessor: "media" },
+    { Header: "Media URL", accessor: "media_url" },
+    { Header: "Start Date", accessor: "start_date" },
+    { Header: "End Date", accessor: "end_date" },
+    { Header: "Status", accessor: "published" },
 ]
 
 function Component() {
