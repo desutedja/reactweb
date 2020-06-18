9a3a0230ff5b4714402f0267a276c5135c14321a
Isa Ansharullah <isa@code.id>
2020-06-18 23:42:21 +0700

    Change product details

--- src/components/UserAvatar.js
+++ src/components/UserAvatar.js
@@ -1,27 +1,23 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import Avatar from 'react-avatar';
 
 function Component({fullname, email, picture=null, round=true}) {
-    const avatar = useMemo(() => {
-        return (<>
-            <Avatar style={{marginRight: '10px'}} size="40" src={picture} 
-                name={fullname} round={typeof round === "boolean" ? true : round} email={email}/>
-            <span> </span>
-        </>)
-    });
-
     return (
-                <>
-                    <div style={{ display: 'flex' }}>
-                        {avatar}
-                        <div style={{ display: 'block' }}>
-                            <div><b>{fullname}</b></div>
-                            <div>{email}</div>
-                        </div>
-                    </div>
-                </> 
+        <>
+            <div style={{ display: 'flex' }}>
+                <Avatar style={{marginRight: '10px'}} size="40" src={picture} 
+                    name={fullname} round={typeof round === "boolean" ? true : round} email={email}/>
+                <span> </span>
+                <div style={{ display: 'block' }}>
+                    <div><b>{fullname}</b></div>
+                    <div>{email}</div>
+                </div>
+            </div>
+        </> 
 
     );
 }
+
+const avatar = React.memo(Component, (oldprops, newprops) => { return oldprops === newprops; })
 
 export default Component;
