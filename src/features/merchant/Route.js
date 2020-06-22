09b8d1464be7f4e9894d00f9bd531880701602b7
Isa Ansharullah <isa@code.id>
2020-06-22 17:25:40 +0700

    Merchant open/close

--- src/features/merchant/Route.js
+++ src/features/merchant/Route.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import { useRouteMatch, Switch, Route, useHistory } from 'react-router-dom';
+import moment from 'moment';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Table from '../../components/Table';
@@ -10,26 +11,35 @@ import Modal from '../../components/Modal';
 import Add from './Add';
 import Details from './Details';
 import { getMerchant, setSelected, deleteMerchant, getMerchantDetails } from './slice';
-import { get, toSentenceCase } from '../../utils';
+import { get, toSentenceCase, dateTimeFormatter } from '../../utils';
 import { merchant_types, endpointMerchant } from '../../settings';
 import { FiSearch } from 'react-icons/fi';
 import { FiPlus } from 'react-icons/fi';
 import { Badge } from 'reactstrap';
+import Tile from '../../components/Tile';
+import Pill from '../../components/Pill';
 
 const columns = [
     { Header: 'ID', accessor: 'id' },
     { Header: 'Name', accessor: 'name' },
-    { Header: 'Type', accessor: 'type' },
-    { Header: 'Category', accessor: 'category' },
-    { Header: 'description', accessor: 'description' },
-    { Header: 'Open', accessor: 'open_at' },
-    { Header: 'Closed', accessor: 'closed_at' },
-    {
-        Header: 'Status', accessor: row =>
-            <h5><Badge pill color={
-                row.status ? 'success' : 'secondary'
-            }>{toSentenceCase(row.status ? row.status : 'inactive')}</Badge></h5>
+    { Header: 'Type', accessor: row => toSentenceCase(row.type) },
+    { Header: 'Category', accessor: row => toSentenceCase(row.category) },
+    //{ Header: 'Description', accessor: 'description' },
+    { Header: 'Open', accessor: row => {
+        return <Tile items={[
+            row.is_open === 1 ? <Pill color="primary">Open</Pill> : <Pill color="secondary">Closed</Pill>,
+            row.is_open === 1 ? <div style={{ paddingTop: '5px', fontSize: ".9em" }}>Until {dateTimeFormatter(row.is_open_until)}
+            </div> : <span></span>,
+        ]}/>
+      }
     },
+    //{ Header: 'Closed', accessor: 'closed_at' },
+    //{
+    //    Header: 'Status', accessor: row =>
+    //        <h5><Badge pill color={
+    //            row.status ? 'success' : 'secondary'
+    //        }>{toSentenceCase(row.status ? row.status : 'inactive')}</Badge></h5>
+    //},
 ]
 
 function Component() {
