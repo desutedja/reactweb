bd855803a53beae235c1d0e07921f9c087846d88
Avid Santiko <avidkucing@gmail.com>
2020-07-07 15:43:20 +0700

    fix merchant name

--- src/components/Form.js
+++ src/components/Form.js
@@ -24,7 +24,7 @@ function Component({ children, onSubmit, loading, isModal=false, showSubmit = tr
                         entry[1].includes('@') ? entry[1] :
                         entry[1].includes('[') ? JSON.parse(entry[1]) :
                         entry[1].includes('/') ? entry[1] :
-                        entry[1].includes(entry[1].match(/[0-9]/g)) ? (parseFloat(entry[1])) :
+                        entry[1].includes(entry[1].match(/[^0-9]/g)) ? (parseFloat(entry[1])) :
                         entry[0].includes('billing_duedate') ? (parseFloat(entry[1])) :
                         entry[0].includes('date') ? (entry[1] + ' 00:00:00') :
                         entry[1].includes(':/') ? entry[1] :
