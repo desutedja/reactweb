cfcd4f3225784e311c8a42454dc6e692a0fcc61f
Avid Santiko <avidkucing@gmail.com>
2020-07-09 13:07:34 +0700

    cherrypick better-forms branch

--- src/features/add/Merchant.js
+++ src/features/add/Merchant.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 
 import Input from "../../components/Input";
-import Form from "../../components/Form";
 import Modal from "../../components/Modal";
 import SectionSeparator from "../../components/SectionSeparator";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import {
-  endpointResident,
   endpointAdmin,
   endpointMerchant,
   banks,
@@ -18,27 +16,71 @@ import { FiMapPin } from "react-icons/fi";
 import { get } from "../slice";
 import Template from "./components/Template";
 
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+
+const defaultRequiredError = 'This field is required.';
+
+const Text = Yup.string().required(defaultRequiredError);
+
+const TextOptional = Yup.string();
+
+const Phone = Yup.string()
+  .matches(/^(0*[1-9][0-9]*(\.[0-9]*)?|0*\.[0-9]*[1-9][0-9]*)$/, "Phone number should not contain unnecesarry characters.")
+  .max(14, "Phone number must be less than 14.")
+  .min(11, "Phone number more than 11.")
+  .required(defaultRequiredError);
+
+const Email = Yup.string().email('Invalid email').required(defaultRequiredError);
+
+const merchantSchema = Yup.object().shape({
+  "name": Text,
+  "phone": Phone,
+  "type": Text,
+  "legal": Text,
+  "category": Text,
+  "status": Text,
+  "description": Text,
+  "in_building": TextOptional,
+  "lat": Text,
+  "long": Text,
+  "address": Text,
+  "pic_name": Text,
+  "pic_phone": Phone,
+  "pic_mail": Email,
+  "account_bank": Text,
+  "account_no": Text,
+  "account_name": Text,
+});
+
+const merchantPayload = {
+  "name": "",
+  "type": "goods",
+  "legal": "individual",
+  "category": "",
+  "phone": "",
+  "status": "inactive",
+  "description": "",
+  "in_building": 0,
+  "address": "",
+  "lat": "",
+  "long": "",
+  "pic_name": "",
+  "pic_phone": "",
+  "pic_mail": "",
+  "account_bank": "",
+  "account_no": "",
+  "account_name": "",
+  "category_label": "",
+  "in_building_label": "None",
+  "account_bank_label": "",
+}
+
 function Component() {
   const [modal, setModal] = useState(false);
-  const [lat, setLat] = useState('');
-  const [lng, setLng] = useState('');
 
-  const [district, setDistrict] = useState("");
-  const [districts, setDistricts] = useState([]);
-
-  const [city, setCity] = useState("");
-
-  const [cities, setCities] = useState([]);
-
-  const [province, setProvince] = useState("");
-  const [provinces, setProvinces] = useState([]);
-
-  const [inBuilding, setBuilding] = useState(0);
   const [inBuildings, setBuildings] = useState([]);
-
-  const [category, setCategory] = useState("");
   const [categories, setCategories] = useState([]);
-
 
   const { loading, selected } = useSelector((state) => state.merchant);
 
@@ -46,7 +88,6 @@ function Component() {
   let history = useHistory();
 
   useEffect(() => {
-    setBuilding(0);
     dispatch(get(endpointAdmin + "/building?page=1&limit=9999", (res) => {
       let formatted = res.data.data.items.map((el) => ({
         label: el.name,
@@ -59,7 +100,6 @@ function Component() {
   }, [dispatch]);
 
   useEffect(() => {
-    setCategory("");
     dispatch(get(endpointMerchant + "/admin/categories", (res) => {
       let formatted = res.data.data.map((el) => ({
         label: el.name,
@@ -69,228 +109,139 @@ function Component() {
     }));
   }, [dispatch]);
 
-  useEffect(() => {
-    dispatch(get(endpointResident + "/geo/province", (res) => {
-      let formatted = res.data.data.map((el) => ({
-        label: el.name,
-        value: el.id,
-      }));
-      setProvinces(formatted);
-    }));
-  }, [dispatch]);
-
-  useEffect(() => {
-    setCity("");
-    (province || selected.province) &&
-      dispatch(get(
-        endpointResident +
-        "/geo/province/" +
-        (province ? province : selected.province),
-
-        (res) => {
-          let formatted = res.data.data.map((el) => ({
-            label: el.name,
-            value: el.id,
-          }));
-          setCities(formatted);
-        }
-      ));
-  }, [dispatch, province, selected.province]);
-
-  useEffect(() => {
-    setDistrict("");
-    (city || selected.city) &&
-      dispatch(get(
-        endpointResident + "/geo/city/" + (city ? city : selected.city),
-
-        (res) => {
-          let formatted = res.data.data.map((el) => ({
-            label: el.name,
-            value: el.id,
-          }));
-          setDistricts(formatted);
-        }
-      ));
-  }, [city, dispatch, selected.city]);
-
-  // useEffect(() => {
-  //   setLat();
-  // }, [inBuilding])
-
   return (
     <Template>
-      <Modal isOpen={modal} toggle={() => {
-        setLat('');
-        setLng('');
-        setModal(false);
-      }} onClick={() => setModal(false)} okLabel={"select"}>
-        <div style={{ height: '40rem', width: '100%' }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyB2COXmiUjYMi651In_irBIHaKnT17L_X8' }}
-            defaultCenter={{
-              lat: -6.210786300000009,
-              lng: 106.81379770000001,
-            }}
-            zoom={12}
-            onClick={({ x, y, lat, lng, event }) => {
-              setLat(lat);
-              setLng(lng);
-              console.log(lat, lng);
-            }}
-            onChange={({ center }) => {
-              setLat(center.lat);
-              setLng(center.lng);
-              console.log(center.lat, center.lng);
-            }}
-          >
-            <div style={{
-              position: 'absolute',
-              transform: 'translate(-50%, -50%)'
-            }}>
-              <FiMapPin size={40} color="dodgerblue" />
-            </div>
-          </GoogleMapReact>
-        </div>
-        <div className="MapForm">
-          <Input label='Latitude' compact inputValue={lat} setInputValue={setLat} />
-          <Input label='Longitude' compact inputValue={lng} setInputValue={setLng} />
-        </div>
-      </Modal>
-      <Form
-        onSubmit={(data) => {
-          // console.log(data)
+      <Formik
+        initialValues={selected.id ? {
+          ...selected,
+          "category_label": selected.category,
+          "in_building_label": selected.in_building,
+          "account_bank_label": selected.account_bank,
+        } : merchantPayload}
+        validationSchema={merchantSchema}
+        onSubmit={(values) => {
+          const data = { ...values, phone: '+62' + values.phone };
+
+          console.log(data);
+
           selected.id ?
             dispatch(editMerchant(data, history, selected.id))
             :
             dispatch(createMerchant(data, history));
         }}
-        loading={loading}
       >
-        <h2
-          style={{
-            color: "gray",
-          }}
-        >
-          Merchant Info
-        </h2>
-        <SectionSeparator />
-        <Input label="Name" inputValue={selected.name} />
-        <Input label="Phone" type="phone" inputValue={selected.phone} />
-        <Input
-          label="Legal"
-          type="radio"
-          options={[
-            { value: "individual", label: "Individu" },
-            { value: "company", label: "Perusahaan" },
-          ]}
-          inputValue={selected.legal}
-        />
-        <Input
-          label="Type"
-          name="type"
-          type="radio"
-          options={[
-            { value: "services", label: "Services" },
-            { value: "goods", label: "Goods" },
-          ]}
-          inputValue={selected.type}
-        />
-        <Input
-          label="Category"
-          type="select"
-          options={categories}
-          inputValue={category ? category : selected.category}
-          setInputValue={setCategory}
-        />
-        <Input
-          label="Province"
-          type="select"
-          options={provinces}
-          inputValue={province ? province : selected.province}
-          setInputValue={setProvince}
-        />
-        <Input
-          label="City"
-          type="select"
-          options={cities}
-          inputValue={city ? city : selected.city}
-          setInputValue={setCity}
-        />
-        <Input
-          label="District"
-          type="select"
-          options={districts}
-          inputValue={district ? district : selected.district}
-          setInputValue={setDistrict}
-        />
-        <Input
-          label="Status"
-          type="radio"
-          options={[
-            { value: "active", label: "Active" },
-            { value: "inactive", label: "Inactive" },
-          ]}
-          inputValue={selected.status}
-        />
-        <Input label="Description" type="textarea" inputValue={selected.description} />
-        <Input
-          optional
-          label="Building"
-          type="select"
-          name="in_building"
-          options={[{
-            label: 'None',
-            value: 0,
-          }, ...inBuildings]}
-          inputValue={inBuilding ? inBuilding : selected.in_building ? selected.in_building : '0'}
-          setInputValue={value => {
-            setBuilding(value);
-            console.log(inBuildings.find(el => el.value === parseInt(value)));
-            setLat(inBuildings.find(el => el.value === parseInt(value))?.lat);
-            setLng(inBuildings.find(el => el.value === parseInt(value))?.long);
-          }}
-        />
-        <Input label="Select Location" type="button"
-          onClick={() => {
-            setLat(-6.2107863);
-            setLng(106.8137977);
-            setModal(true);
-          }}
-          hidden={inBuilding}
-        />
-        <Input label="Latitude" name="lat" inputValue={lat ? lat : selected.lat}
-          setInputValue={setLat}
-        />
-        <Input label="Longitude" name="long" inputValue={lng ? lng : selected.long}
-          setInputValue={setLng}
-        />
-        <Input label="Address" type="textarea" inputValue={selected.address} />
-        <SectionSeparator />
-        <h2
-          style={{
-            color: "gray",
-          }}
-        >
-          PIC Info
-        </h2>
-        <SectionSeparator />
-        <Input label="Name" name="pic_name" inputValue={selected.pic_name} />
-        <Input label="Phone" name="pic_phone" inputValue={selected.pic_phone} />
-        <Input label="Email" name="pic_mail" inputValue={selected.pic_mail} />
-        <SectionSeparator />
-        <h2
-          style={{
-            color: "gray",
-          }}
-        >
-          Account Info
-        </h2>
-        <SectionSeparator />
-        <Input label="Account No" name="account_no" inputValue={selected.account_no} />
-        <Input label="Account Name" name="account_name" inputValue={selected.account_name} />
-        <Input label="Account Bank" name="account_bank" type="select" options={banks} inputValue={selected.account_bank} />
-        <SectionSeparator />
-      </Form>
+        {props => {
+          const { setFieldValue, values } = props;
+          // console.log(rest);
+          return (
+            <Form className="Form">
+              <Modal isOpen={modal} toggle={() => {
+                setModal(false);
+              }} onClick={() => setModal(false)} okLabel={"Select"}>
+                <div style={{ height: '40rem', width: '100%' }}>
+                  <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyB2COXmiUjYMi651In_irBIHaKnT17L_X8' }}
+                    defaultCenter={{
+                      lat: -6.2107863,
+                      lng: 106.8137977,
+                    }}
+                    zoom={12}
+                    onClick={({ x, y, lat, lng, event }) => {
+                      setFieldValue('lat', lat);
+                      setFieldValue('long', lng);
+                      console.log(lat, lng);
+                    }}
+                    onChange={({ center }) => {
+                      setFieldValue('lat', center.lat);
+                      setFieldValue('long', center.lng);
+                      console.log(center.lat, center.lng);
+                    }}
+                  >
+                    <div style={{
+                      position: 'absolute',
+                      transform: 'translate(-50%, -50%)'
+                    }}>
+                      <FiMapPin size={40} color="dodgerblue" />
+                    </div>
+                  </GoogleMapReact>
+                </div>
+              </Modal>
+
+              <Input {...props} label="Name" />
+              <Input {...props} label="Phone" prefix="+62" />
+              <Input {...props} label="Type"
+                type="radio"
+                options={[
+                  { value: "goods", label: "Goods" },
+                  { value: "services", label: "Services" },
+                ]}
+              />
+              <Input {...props} label="Legal"
+                type="radio"
+                options={[
+                  { value: "individual", label: "Individu" },
+                  { value: "company", label: "Perusahaan" },
+                ]}
+              />
+              <Input {...props}
+                label="Category"
+                type="select"
+                options={categories}
+              />
+              <Input {...props} label="Status" type="radio"
+                options={[
+                  { value: "active", label: "Active" },
+                  { value: "inactive", label: "Inactive" },
+                ]}
+              />
+              <Input {...props} label="Description" type="textarea" />
+
+              <SectionSeparator />
+
+              <Input
+                {...props}
+                optional
+                label="Building"
+                type="select"
+                name="in_building"
+                options={inBuildings}
+                onChange={el => {
+                  console.log(el);
+                  setFieldValue('lat', el.lat);
+                  setFieldValue('long', el.long);
+                }}
+              />
+              {!values['in_building'] &&
+                <button type="button" onClick={() => setModal(true)}>
+                  Select Location
+                </button>
+              }
+              <Input {...props} label="Latitude" name="lat" />
+              <Input {...props} label="Longitude" name="long" />
+              <Input {...props} label="Address" type="textarea" />
+
+              <SectionSeparator />
+
+              <Input {...props} label="PIC Name" />
+              <Input {...props} label="PIC Phone" prefix="+62" />
+              <Input {...props} label="PIC Mail" />
+
+              <SectionSeparator />
+
+              <Input {...props} label="Bank Account" name="account_bank" options={banks} />
+              <Input {...props} label="Bank Account Number" name="account_no" />
+              <Input {...props} label="Bank Account Name" name="account_name" />
+              {!loading &&
+                <button type="submit"
+                  onClick={() => console.log(values)}
+                >
+                  Submit
+                </button>
+              }
+            </Form>
+          )
+        }}
+      </Formik>
     </Template>
   );
 }
