5da0a5e572b62d0e88ea23fcde7a2a74d890a600
Isa Ansharullah <isa@code.id>
2020-07-16 23:15:37 +0700

    Add confirmation for building modules

--- src/features/details/Building/contents/Module.js
+++ src/features/details/Building/contents/Module.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
+import Modal from '../../../../components/Modal';
 import { get, post } from '../../../slice';
 import { endpointAdmin } from '../../../../settings';
 import ClinkLoader from '../../../../components/ClinkLoader';
@@ -27,12 +28,14 @@ const modulesFiltered = (arrA, arrB) => {
   for (let i = 0; i < arrA.length; i++) {
     const truthy = arrB.map(b => b === arrA[i].value).find(bool => bool === true)
     res.push(
-      {label: arrA[i].label, value: truthy || false}
+        {label: arrA[i].label, truthvalue: truthy || false,
+            value: arrA[i].value}
     );
   }
   return res;
 }
 
+    /*
 function useDidUpdateEffect(fn, inputs) {
   const didMountRef = useRef(false);
 
@@ -44,6 +47,7 @@ function useDidUpdateEffect(fn, inputs) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, inputs);
 }
+*/
 
 export default (props) => {
   const {auth} = useSelector(state => state)
@@ -52,34 +56,45 @@ export default (props) => {
   const [activeModules, setActiveModules] = useState([]);
   const [modulesLabel, setModulesLabel] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [clickedModule, setClickedModule] = useState('');
+  const [confirmChange, setConfirmChange] = useState(false);
+  const [refresh, setRefresh] = useState(true);
 
   useEffect(() => {
     if (auth.role !== 'sa') return;
-    dispatch(get(endpointAdmin + '/modules/building?id=' + id,
+    refresh && dispatch(get(endpointAdmin + '/modules/building?id=' + id,
       res => {
         setActiveModules(res.data.data.active_modules)
         setModulesLabel(modulesFiltered(modules, res.data.data.active_modules))
         setIsLoading(false);
+        setRefresh(false);
       },
       err => console.log(err.response)
       ))
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [auth.role, id])
+  }, [auth.role, id, refresh])
 
-  function toggleButton() {
+  function submitChange() {
     if (auth.role !== 'sa') return;
       setIsLoading(true)
+
+      const updated = activeModules.find(am => am === clickedModule.value) ?
+      activeModules.filter(am => am !== clickedModule.value) : 
+      [clickedModule.value, ...activeModules]
+
       dispatch(post(endpointAdmin + '/modules/building?id=' + id,
         {
-          active_modules: activeModules
+          active_modules: updated
         },
         res => {
+          setConfirmChange(false);
           setIsLoading(false)
+          setRefresh(true);
         }
       ))
   }
 
-  useDidUpdateEffect(toggleButton, [activeModules])
+   //useDidUpdateEffect(submitChange, [activeModules])
   
   // useEffect(() => {
   //   if (auth.role !== 'sa') return;
@@ -99,20 +114,47 @@ export default (props) => {
 
   return (
     <>
+        <Modal 
+            isOpen={confirmChange}
+            disableHeader={true}
+            onClick={submitChange}
+            toggle={() => { 
+                setConfirmChange(false)
+                setModulesLabel(modulesLabel.map((el) => {
+                    if (el.value === clickedModule.value) {
+                        return { value: el.value, truthvalue: clickedModule.truthvalue, label: el.label  }
+                    } else {
+                        return el
+                    }
+                }))
+            } }
+            okLabel={"Confirm"}
+            cancelLabel={"Cancel"}
+        >
+            Are you sure you want to {activeModules.find(am => am === clickedModule.value) === clickedModule.value? "enable" : "disable"} <b>{clickedModule.label}</b> module in this building? 
+        </Modal>
       <div className="row no-gutters mt-4">
         {modulesLabel && modulesLabel.map((el, i) => (
         <div key={i}
         className="col-12 col-sm-6 col-md-6 col-lg
         d-flex justify-content-center p-2">
-          <SwitchBox
-            value={el.value}
-            onClick={e => {
-              activeModules.find(am => am === e) ?
-              setActiveModules(activeModules.filter(am => am !== e)) :
-              setActiveModules([e, ...activeModules])
-            }}
-            label={el.label}
-          />
+          <div className={"switch-box " + (el.truthvalue ? 'switch-on' : 'switch-off')}>
+            <input
+              className="" type="checkbox"
+              id={el.label.toLowerCase()}
+              value={el.truthvalue}
+              onClick={e => {
+                  setClickedModule(el);
+                  setConfirmChange(true);
+               }}
+            />
+            <label
+              className="px-4 py-3"
+              htmlFor={el.label.toLowerCase()}
+            >
+              <span className="text-center">{el.label}: {el.truthvalue ? 'on' : 'off'}</span>
+            </label>
+          </div>
         </div>
         ))}
         {isLoading && (
@@ -121,34 +163,6 @@ export default (props) => {
           <span className="mt-2 text-secondary">please wait...</span>
         </div>
         )}
-      </div>
-    </>
-  )
-}
-
-const SwitchBox = ({
-  label = '', value, onClick, ...rest
-}) => {
-
-  const [inputValue, setInputValue] = useState((value && value) || false);
-
-  return (
-    <>
-      <div className={"switch-box " + (inputValue ? 'switch-on' : 'switch-off')}>
-        <input
-          className="" type="checkbox"
-          id={label.toLowerCase()}
-          value={inputValue}
-          onClick={() => setInputValue(!inputValue)}
-          {...rest}
-        />
-        <label
-          className="px-4 py-3"
-          htmlFor={label.toLowerCase()}
-          onClick={() => onClick(label.toLowerCase())}
-        >
-          <span className="text-center">{label}: {inputValue ? 'on' : 'off'}</span>
-        </label>
       </div>
     </>
   )
