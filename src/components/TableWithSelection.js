ab1a364772e3e88c2c93897d03c867e780e42338
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2020-07-24 16:49:24 +0700

    useless filter container tablewithselection

--- src/components/TableWithSelection.js
+++ src/components/TableWithSelection.js
@@ -199,7 +199,7 @@ function Component({
                     </div>
                 </div>
             </div>
-            <div className={"FilterContainer" + (filter ? ' down' : '')}>
+            {filters.length > 0 && <div className={"FilterContainer" + (filter ? ' down' : '')}>
                 {filters.map((el, index) => !el.hidden &&
                     <FilterButton
                         key={index}
@@ -213,7 +213,7 @@ function Component({
                         }}
                         onClickDelete={el.delete} />
                 )}
-            </div>
+            </div>}
             <div className="Table-content scroller">
                 <table {...getTableProps()}>
                     {loading &&
