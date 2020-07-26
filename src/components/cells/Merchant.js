23fa86c90ceb42bebee916434188088d94f7feeb
Isa Ansharullah <isa@code.id>
2020-07-27 06:42:04 +0700

    Merchant cells to link

--- src/components/cells/Merchant.js
+++ src/components/cells/Merchant.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { setSelected } from '../../features/slices/merchant';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function Component({ items, id, data }) {
-    let dispatch = useDispatch();
-    let history = useHistory();
 
-    return <div style={{ display: "block" }} className="Item" onClick={() => {
-            history.push({
-                pathname: 'merchant/' + id,
-                state: data
-            });
-            dispatch(setSelected(data));
-        }}>
-        { items.map((el, i) =>
-            <div key={i}>
-                {i === 0 ? <b>{el}</b> : <>{el}</>}
-            </div>
-        ) }
-    </div>
+    const { role } = useSelector(state => state.auth);
+
+    return <Link to={"/" + role + "/merchant/" + id} style={{ display: "block", textDecoration:"none", color:"#00000" }} 
+                className="Item">
+            { items.map((el, i) =>
+                <div key={i}>
+                    {i === 0 ? <b>{el}</b> : <>{el}</>}
+                </div>
+            ) }
+            </Link>
 }
 
 export default Component;
