77d52cfc1f8c28cef5fb339e8bbc36b01e442438
Isa Ansharullah <isa@code.id>
2020-07-30 21:26:01 +0700

    task cells linkify, add margin left on detail task/trx

--- src/components/cells/Task.js
+++ src/components/cells/Task.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
 import { getTaskDetails } from '../../features/slices/task';
 
 function Component({ items, id, data }) {
     let dispatch = useDispatch();
-    let history = useHistory();
 
-    return <div style={{ display: "block" }} className="Item" onClick={() => {
-            history.push({
-                pathname: 'task/' + id,
-                state: data
-            });
-            dispatch(getTaskDetails(data, history))
-        }}>
+    const { role } = useSelector(state => state.auth);
+
+    return <Link to={"/" + role + "/task/" + id} style={{ display: "block" }} className="Item">
         { items.map((el, i) =>
             <div key={i}>
                 {i === 0 ? <b>{el}</b> : <>{el}</>}
             </div>
         ) }
-    </div>
+    </Link>
 }
 
 export default Component;
