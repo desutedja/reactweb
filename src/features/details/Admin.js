90add8abf0c75ae3df520ea8fd9a7ef0a28b7dfa
Avid Santiko <avidkucing@gmail.com>
2020-08-04 21:16:24 +0700

    fixes

--- src/features/details/Admin.js
+++ src/features/details/Admin.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 
 import { get, setConfirmDelete } from '../slice';
