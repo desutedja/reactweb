81084d38eb4ea70c12e6bea022e15eced5614259
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2020-08-11 16:05:13 +0700

    Fix show submenu list transaction on SA - finance

--- src/features/roles/Finance/Transaction.js
+++ src/features/roles/Finance/Transaction.js
@@ -2,6 +2,7 @@ import React, {  } from 'react';
 
 import { useRouteMatch, Switch, Route, Redirect } from 'react-router-dom';
 
+import List from '../../list/Transaction';
 import Settlement from '../../settlement/Transaction';
 import Disbursement from '../../disbursement/Transaction';
 import Details from '../../details/Transaction';
@@ -13,6 +14,9 @@ function Component() {
     return (
             <Switch>
                 <Redirect exact from={path} to={`${path}/settlement`} />
+                <Route path={`${path}/list`}>
+                    <List />
+                </Route>
                 <Route path={`${path}/settlement`}>
                     <Settlement />
                 </Route>
