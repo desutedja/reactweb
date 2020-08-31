883b8d452731a9516c647427f47c156da2632d01
Avid Santiko <avidkucing@gmail.com>
2020-08-31 12:55:19 +0700

    fix button color

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'reactstrap';
 
 function MyButton({ icon, label, onClick, selected, color, disabled }) {
+
     return (
         <Button
             className="Button"
@@ -12,6 +13,7 @@ function MyButton({ icon, label, onClick, selected, color, disabled }) {
                 marginRight: 4,
                 marginLeft: 4,
                 marginBottom: 8,
+                backgroundColor: color ? '' : '#244091',
             }}
             type="submit"
         >
