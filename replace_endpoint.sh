37f01f54c78152a08abe79d010020dac517beba2
Isa Ansharullah <isa@code.id>
2020-09-17 13:40:38 +0700

    Also change the script

--- replace_endpoint.sh
+++ replace_endpoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 export TEMPLATE=`cat <<EOF
+const root = '${ENDPOINT_PREFIX}';
 export const _endpointBase = 'https://${ENDPOINT_BASE}';
-export const _endpointAsset = _endpointBase + '/${ENDPOINT_PREFIX}-assets';
-export const _endpointAdmin = _endpointBase + '/${ENDPOINT_PREFIX}-admin';
-export const _endpointBilling = _endpointBase + '/${ENDPOINT_PREFIX}-billing';
-export const _endpointResident = _endpointBase + '/${ENDPOINT_PREFIX}-resident';
-export const _endpointManagement = _endpointBase + '/${ENDPOINT_PREFIX}-management';
-export const _endpointTask = _endpointBase + '/${ENDPOINT_PREFIX}-task';
-export const _endpointMerchant = _endpointBase + '/${ENDPOINT_PREFIX}-merchant';
-export const _endpointTransaction = _endpointBase + '/${ENDPOINT_PREFIX}-transaction';
-export const _endpointAds = _endpointBase + '/${ENDPOINT_PREFIX}-advertisement';
+
+export const _endpointAsset = _endpointBase + '/' + root + '-assets';
+export const _endpointAdmin = _endpointBase + '/' + root + '-admin';
+export const _endpointBilling = _endpointBase + '/' + root + '-billing';
+export const _endpointResident = _endpointBase + '/' + root + '-resident';
+export const _endpointManagement = _endpointBase + '/' + root + '-management';
+export const _endpointTask = _endpointBase + '/' + root + '-task';
+export const _endpointMerchant = _endpointBase + '/' + root + '-merchant';
+export const _endpointTransaction = _endpointBase + '/' + root + '-transaction';
+export const _endpointAds = _endpointBase + '/' + root + '-advertisement';
 EOF`
 
 echo "$TEMPLATE" > src/endpoints.js
