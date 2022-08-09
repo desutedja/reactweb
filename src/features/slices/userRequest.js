c92423c5a281279900fed553fc8cc34e5374bccc
juniar <mjuniarnurfirdaus@sentra-ip.co.id>
2022-08-09 19:20:37 +0700

    add asset image android & iOS for booking form, adding style at input.css, change button color button submit, update endpoint userRequest

--- src/features/slices/userRequest.js
+++ src/features/slices/userRequest.js
@@ -54,50 +54,33 @@ export const {
 
 export default slice.reducer;
 
-// export const getInternetProvider = ( pageIndex, pageSize, search = '', role, provider = "") => dispatch => {
-//   dispatch(startAsync());
+export const getUserRequest = ( pageIndex, pageSize, search = '', cat, subCat, stat = '') => dispatch => {
+  dispatch(startAsync());
 
-//   dispatch(get(internetEndpoint + '/provider' +
-//     '?page=' + (pageIndex + 1) +
-//     '&limit=' + pageSize +
-//     '&search=' + search +
-//     '&provider_id=' + provider,
+  dispatch(get(endpointUserRequest + "/data/list" +
+    "?page=" +
+    (pageIndex + 1) +
+    "&limit=" +
+    pageSize +
+    "&category_id=" +
+    cat +
+    "&sub_category_id=" +
+    subCat +
+    "&search=" +
+    search +
+    "&status=" +
+    stat,
     
-//     res => {
-//       dispatch(setData(res.data.data));
-//       console.log(res);
+    (res) => {
+      dispatch(setData(res.data.data));
+      console.log(res);
 
-//       dispatch(stopAsync());
-//     },
-//     err => {
-//       dispatch(stopAsync());
-//     }))
-// }
-
-// export const downloadStaff = ( pageIndex, pageSize, role, building, shift, search = '', department, management = '') => dispatch => {
-//   dispatch(startAsync());
-
-//   dispatch(getFile(staffEndpoint + '/list' +
-//     '?page=' + (pageIndex + 1) +
-//     '&limit=101' + 
-//     '&search=' + search +
-//     '&building_id=' + building +
-//     '&department_ids=' + department +
-//     '&staff_all=1' +
-//     '&is_shift=' + (shift === 'yes' ? 1 : shift === 'no' ? 0 : '') +
-//     '&staff_role=' + role +
-//     // '&sort_field=created_on&sort_type=DESC' +
-//     '&management=' + management +
-//     '&download=1',
-//     "Data_Staff.csv",
-    
-//     (res) => {
-//       dispatch(stopAsync());
-//     },
-//     (err) => {
-//       dispatch(stopAsync());
-//     }))
-// }
+      dispatch(stopAsync());
+    },
+    err => {
+      dispatch(stopAsync());
+    }))
+}
 
 export const createUserRequest = (data, history) => (dispatch, getState) => {
   dispatch(startAsync());
@@ -188,61 +171,3 @@ export const deleteUserRequest = (row, history) => (dispatch, getState) => {
     )
   );
 };
-
-// export const editStaff = ( data, history, id) => dispatch => {
-//   dispatch(startAsync());
-
-//   dispatch(put(staffEndpoint + '/update', { ...data, id: id }, 
-//     res => {
-//       // dispatch(setSelected(res.data.data));
-//       history.push(`${id}`);
-
-//       dispatch(setInfo({
-//         color: 'success',
-//         message: 'Staff has been updated.'
-//       }));
-
-//       dispatch(stopAsync());
-//     },
-//     err => {
-//       dispatch(stopAsync());
-//     }))
-// }
-
-// export const deleteStaff = (row, history) => (dispatch, getState) => {
-//   dispatch(startAsync());
-
-//   const { auth } = getState();
-
-//   dispatch(del(staffEndpoint + '/delete?id=' + row.id, 
-//     res => {
-//       history && history.push('/' + auth.role + '/staff');
-
-//       dispatch(refresh());
-      
-//       dispatch(setInfo({
-//         color: 'success',
-//         message: 'Staff has been deleted.'
-//       }));
-
-//       dispatch(stopAsync())
-//     },
-//     err => {
-//       dispatch(stopAsync());
-//     }))
-// }
-
-// export const getStaffDetails = (row,  history, url) => dispatch => {
-//   dispatch(startAsync());
-
-//   dispatch(get(staffEndpoint + '/' + row.id, 
-//     res => {
-//       dispatch(setSelected(res.data.data));
-//       history.push(url + '/' + row.id);
-
-//       dispatch(stopAsync())
-//     },
-//     err => {
-//       dispatch(stopAsync());
-//     }))
-// }
