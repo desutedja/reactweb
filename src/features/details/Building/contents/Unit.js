176814906a21a3ecc233f8d5d3beeeb1759c4a7f
juniar <mjuniarnurfirdaus@sentra-ip.co.id>
2022-12-29 06:58:46 +0700

    Update detail building unit tab
    memunculkan action popup untuk Menampilkan Informasi Resident pada Halaman Building > Detail Building > unit
    Fix download template bulk unit tidak berfungsi

--- src/features/details/Building/contents/Unit.js
+++ src/features/details/Building/contents/Unit.js
@@ -6,6 +6,8 @@ import Table from "../../../../components/Table";
 import Button from "../../../../components/Button";
 import Modal from "../../../../components/Modal";
 import Input from "../../../../components/Input";
+import TwoColumnResident from "../../../../components/TwoColumnResident";
+import ListResident from "../../../../components/cells/ListResident";
 import { toSentenceCase } from "../../../../utils";
 import {
   deleteBuildingUnit,
@@ -15,34 +17,12 @@ import {
   getBuildingSection,
   getBuildingUnitType,
 } from "../../../slices/building";
-import { setConfirmDelete } from "../../../slice";
-import UploadModal from "../../../../components/UploadModal";
+import { get, getErr, setConfirmDelete } from "../../../slice";
 import { endpointAdmin } from "../../../../settings";
+import UploadModal from "../../../../components/UploadModal";
+import Avatar from "react-avatar";
 
-const columnsUnit = [
-  { Header: "ID", accessor: "id" },
-  { Header: "Number", accessor: "number" },
-  { Header: "Floor", accessor: "floor" },
-  {
-    Header: "Section",
-    accessor: (row) =>
-      toSentenceCase(row.section_type) + " " + row.section_name,
-  },
-  {
-    Header: "Type",
-    accessor: (row) => (
-      <div>
-        {(row.unit_type_name.length > 3
-          ? toSentenceCase(row.unit_type_name)
-          : row.unit_type_name.toUpperCase()) + " - "}
-        {row.unit_size + " m"}
-        <sup>2</sup>
-      </div>
-    ),
-  },
-];
-
-function Component({ view, canUpdate, canDelete, canAdd }) {
+function Component({ view, canUpdate, canDelete, canAdd, data }) {
   const [selectedRow, setRow] = useState({});
   const [edit, setEdit] = useState(false);
   const [addUnit, setAddUnit] = useState(false);
@@ -53,29 +33,67 @@ function Component({ view, canUpdate, canDelete, canAdd }) {
   const [number, setNumber] = useState("");
   const [upload, setUpload] = useState(false);
 
-  const {
-    selected,
-    loading,
-    unit,
-    section,
-    unit_type,
-    refreshToggle,
-  } = useSelector((state) => state.building);
+  const [detailResident, setDetailResident] = useState([]);
+  const [idUnit, setIdUnit] = useState();
+  const [residentModal, setResidentModal] = useState(false);
+
+  const { selected, loading, unit, section, unit_type, refreshToggle } =
+    useSelector((state) => state.building);
   const { user } = useSelector((state) => state.auth);
+
+  const columnsUnit = [
+    { Header: "ID", accessor: (row) => row.id },
+    {
+      Header: "Number",
+      accessor: (row) => <ListResident id={row.id} data={row} />,
+    },
+    { Header: "Floor", accessor: "floor" },
+    {
+      Header: "Section",
+      accessor: (row) =>
+        toSentenceCase(row.section_type) + " " + row.section_name,
+    },
+    {
+      Header: "Type",
+      accessor: (row) => (
+        <div>
+          {(row.unit_type_name.length > 3
+            ? toSentenceCase(row.unit_type_name)
+            : row.unit_type_name.toUpperCase()) + " - "}
+          {row.unit_size + " m"}
+          <sup>2</sup>
+        </div>
+      ),
+    },
+  ];
 
   let dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getBuildingSection(0, 100, "", selected));
-    dispatch(getBuildingUnitType(0, 100, "", selected));
+    dispatch(getBuildingUnitType(0, 1000, "", selected));
   }, [dispatch, selected]);
+
+  useEffect(() => {
+    dispatch(
+      getErr(
+        endpointAdmin + "/building/list/resident_unit?" + "unit_id=" + idUnit,
+
+        (res) => {
+          setDetailResident(res.data.data.items);
+        }
+      )
+    );
+  }, [dispatch, idUnit]);
 
   return (
     <>
       <UploadModal
         open={upload}
         toggle={() => setUpload(false)}
-        templateLink={user.unit_bulk_template}
+        templateLink={
+          "https://api.yipy.id/yipy-assets/asset-storage/document/E966EC86802F6640240FFA08D7B6C004.xlsx"
+        }
         filename="building_unit_template.xlsx"
         uploadLink={endpointAdmin + "/management/building/bulk_unit"}
         uploadDataName="unit"
@@ -123,12 +141,20 @@ function Component({ view, canUpdate, canDelete, canAdd }) {
         }}
       >
         <form>
-          <Input
-            label="Number"
-            inputValue={selectedRow.number ? selectedRow.number : number}
-            setInputValue={setNumber}
-            disabled={edit}
-          />
+          { user.role === "sa" ?
+            <Input
+              label="Number"
+              inputValue={selectedRow.number ? selectedRow.number : number}
+              setInputValue={setNumber}
+            />
+            :
+            <Input
+              label="Number"
+              inputValue={selectedRow.number ? selectedRow.number : number}
+              setInputValue={setNumber}
+              disabled={edit}
+            />
+          }
           <Input
             label="Floor"
             inputValue={selectedRow.floor ? selectedRow.floor : floor}
@@ -175,19 +201,145 @@ function Component({ view, canUpdate, canDelete, canAdd }) {
           ></div>
         </form>
       </Modal>
+
+      <Modal
+        width="660px"
+        isOpen={residentModal}
+        title="Resident Unit"
+        toggle={() => {
+          setResidentModal(false);
+          setIdUnit();
+        }}
+        cancelLabel={"Close"}
+        disablePrimary
+      >
+        {detailResident.length > 0 ? (
+          <>
+            <div className="p-2">
+              <div
+                style={{
+                  paddingBottom: 10,
+                  fontSize: 12,
+                  fontWeight: 600,
+                  color: "#838799",
+                }}
+                className="title-resident-detail"
+              >
+                Main Account
+              </div>
+              {detailResident.map((item) => (
+                <div>
+                  {item.status === "Owner" ? (
+                    <>
+                      <div className="Item-resident">
+                        <Avatar
+                          className="Item-avatar"
+                          size="40"
+                          src={item.photo}
+                          name={item.firstname + " " + item.lastname}
+                          round
+                          email={item.photo ? null : item.email}
+                        />
+                        <>
+                          <span> </span>
+                          <TwoColumnResident
+                            first={
+                              <div>
+                                <b>{item.firstname + " " + item.lastname}</b>
+                                <p className="Item-subtext-resident">
+                                  {item.phone} {item.phone ? "|" : []}{" "}
+                                  {item.email}
+                                </p>
+                              </div>
+                            }
+                            second={
+                              <Button
+                                color="Activated"
+                                label={item.status ? item.status : "-"}
+                              />
+                            }
+                          />
+                        </>
+                      </div>
+                    </>
+                  ) : (
+                    []
+                  )}
+                </div>
+              ))}
+              <hr />
+            </div>
+            <div className="p-2">
+              <div
+                style={{
+                  paddingBottom: 10,
+                  fontSize: 12,
+                  fontWeight: 600,
+                  color: "#838799",
+                }}
+                className="title-resident-detail"
+              >
+                Sub Account
+              </div>
+              {detailResident.map((item) => (
+                <div>
+                  {item.status !== "Owner" ? (
+                    <>
+                      <div className="Item-resident">
+                        <Avatar
+                          className="Item-avatar"
+                          size="40"
+                          src={item.photo}
+                          name={item.firstname + " " + item.lastname}
+                          round
+                          email={item.photo ? null : item.email}
+                        />
+                        <>
+                          <span> </span>
+                          <TwoColumnResident
+                            first={
+                              <div>
+                                <b>{item.firstname + " " + item.lastname}</b>
+                                <p className="Item-subtext-resident">
+                                  {item.phone} {item.phone ? "|" : []}{" "}
+                                  {item.email}
+                                </p>
+                              </div>
+                            }
+                            second={
+                              <Button
+                                color="Activated"
+                                label={item.status ? item.status : "-"}
+                              />
+                            }
+                          />
+                        </>
+                      </div>
+                    </>
+                  ) : (
+                    []
+                  )}
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          "No resident found for this unit"
+        )}
+      </Modal>
       <Table
         noContainer={true}
         columns={columnsUnit}
         data={unit.items}
         loading={loading}
         pageCount={unit.total_pages}
-        totalItems={unit.items.length}
         fetchData={useCallback(
           (pageIndex, pageSize, search) =>
             dispatch(getBuildingUnit(pageIndex, pageSize, search, selected)),
           // eslint-disable-next-line react-hooks/exhaustive-deps
           [dispatch, selected, refreshToggle, upload]
         )}
+        totalItems={unit.total_items}
         filters={[]}
         actions={
           view
@@ -215,6 +367,16 @@ function Component({ view, canUpdate, canDelete, canAdd }) {
                   onClick={() => setUpload(true)}
                 />,
               ]
+            : null
+        }
+        onClickList={
+          view
+            ? null
+            : canUpdate
+            ? (row) => {
+                setIdUnit(row.id);
+                setResidentModal(true);
+              }
             : null
         }
         onClickDelete={
