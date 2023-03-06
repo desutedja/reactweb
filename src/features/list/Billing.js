458d644cbbc0d0b4e4da393bc8f8e5c41a977987
Fatan Najuda <fatan@sentra-ip.co.id>
2023-03-06 16:00:54 +0700

    T-Yippy 1919 Download Template Set as Paid Khusus RMG Diganti Dengan yang Baru

--- src/features/list/Billing.js
+++ src/features/list/Billing.js
@@ -3143,7 +3143,9 @@ function Component({ view }) {
         open={uploadSetAsPaid}
         toggle={() => setUploadSetAsPaid(false)}
         templateLink={
-          "https://api.yipy.id/yipy-assets/asset-storage/document/8D23E9158CBE5501CFDBD34E4B132C54.xlsx"
+          role != "sa" && buildingBM == 19 ? 
+          "https://api.yipy.id/yipy-assets/asset-storage/document/D591544D0A4B104DDB07717153F142D5.xlsx" : building == 19 ?
+          "https://api.yipy.id/yipy-assets/asset-storage/document/D591544D0A4B104DDB07717153F142D5.xlsx" : "https://api.yipy.id/yipy-assets/asset-storage/document/8D23E9158CBE5501CFDBD34E4B132C54.xlsx"
         }
         filename="set_as_paid_template.xlsx"
         uploadLink={
